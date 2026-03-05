b42b4998031520746abe93c77b4d9041ef395454
João Igor <gembra10ecarlos@gmail.com>
2026-03-05 19:41:51 -0300

    corrigindo base do vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins:[react()],
-  base:"/Cturismo_Fullstack_SQLite/"
+  plugins: [react()],
+  base: "/cturismo-full/"
 })
